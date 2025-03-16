acb3bc623314d497fe1232a2c5e1a9b66631ebe4
Matthew Cross <matthew.cross@oxinst.com>
2025-03-16 15:50:37 +0000

    Initial commit

---
+++ TrafficLights/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace TrafficLights
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static readonly int MIN_LIGHTS = 2;
+        static readonly int MAX_LIGHTS = 4;
+        static readonly int MIN_CARS = 1;
+        static readonly int MAX_CARS = 5;
+
+        DispatcherTimer lightsTimer = new DispatcherTimer();
+
+        int currentLight = 0;
+        int numberOfCars = 2;
+        int numberOfLights = 2;
+        int lightTimerVal = 0;
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+
+            lightTimerVal = 2000 + (numberOfCars * 1000);
+            lightsTimer.Tick += new EventHandler(LightsTimer_Tick);
+            lightsTimer.Interval = new TimeSpan(0, 0, 0, 0, lightTimerVal);
+            lightsTimer.Start();
+
+            masterLight.SlaveLight = slaveLight1;
+            UpdateLights();
+            UpdateCars();
+        }
+
+        private void LightsTimer_Tick(object sender, EventArgs e)
+        {
+            switch (currentLight++)
+            {
+                case 0:
+                    masterLight.NextState();
+                    break;
+
+                case 1:
+                    masterLight.SlaveLight.NextState();
+                    break;
+
+                case 2:
+                    masterLight.SlaveLight.SlaveLight.NextState();
+                    break;
+
+                case 3:
+                    masterLight.SlaveLight.SlaveLight.SlaveLight.NextState();
+                    break;
+            }
+
+            currentLight %= numberOfLights;
+        }
+
+        private void Button_DecClick(object sender, RoutedEventArgs e)
+        {
+            if (numberOfCars > MIN_CARS)
+            {
+                numberOfCars--;
+                lightTimerVal = 2000 + (numberOfCars * 1000);
+                lightsTimer.Interval = new TimeSpan(0, 0, 0, 0, lightTimerVal);
+                UpdateCars();
+            }
+        }
+
+        private void Button_IncClick(object sender, RoutedEventArgs e)
+        {
+            if (numberOfCars < MAX_CARS)
+            {
+                numberOfCars++;
+                lightTimerVal = 2000 + (numberOfCars * 1000);
+                lightsTimer.Interval = new TimeSpan(0, 0, 0, 0, lightTimerVal);
+                UpdateCars();
+            }
+        }
+
+        private void Button_DecLightsClick(object sender, RoutedEventArgs e)
+        {
+            if (numberOfLights > MIN_LIGHTS)
+            {
+                numberOfLights--;
+                UpdateLights();
+            }
+        }
+
+        private void Button_IncLightsClick(object sender, RoutedEventArgs e)
+        {
+            if (numberOfLights < MAX_LIGHTS)
+            {
+                numberOfLights++;
+                UpdateLights();
+            }
+        }
+
+        private void UpdateCars()
+        {
+            lblLightTimerVal.Content = "Timer Val(ms): " + lightTimerVal.ToString();
+            lblNoCars.Content = "Number of Cars: " + numberOfCars.ToString();
+            for (int i = 1; i <= MAX_CARS; i++)
+            {
+                string name = "CarRight" + i.ToString();
+                object item = mainRoad.FindName(name);
+                ((Image)item).Visibility = i < numberOfCars + 1 ? Visibility.Visible : Visibility.Hidden;
+
+                name = "CarLeft" + i.ToString();
+                item = mainRoad.FindName(name);
+                ((Image)item).Visibility = i < numberOfCars + 1 ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+
+        private void UpdateLights()
+        {
+            lblNoLights.Content = "Number of Lights: " + numberOfLights.ToString();
+            switch (numberOfLights)
+            {
+                case 2:
+                    masterLight.SlaveLight = slaveLight1;
+                    slaveLight2.Visibility = Visibility.Hidden;
+                    slaveLight3.Visibility = Visibility.Hidden;
+                    break;
+
+                case 3:
+                    slaveLight1.SlaveLight = slaveLight2;
+                    slaveLight2.Visibility = Visibility.Visible;
+                    slaveLight3.Visibility = Visibility.Hidden;
+                    break;
+
+                case 4:
+                    slaveLight1.SlaveLight = slaveLight2;
+                    slaveLight2.Visibility = Visibility.Visible;
+                    slaveLight2.SlaveLight = slaveLight3;
+                    slaveLight3.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+    }
+}
---
+++ TrafficLights/Views/TrafficLightView.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace TrafficLights.Views
+{
+    /// <summary>
+    /// Interaction logic for TrafficLightView.xaml
+    /// </summary>
+    public partial class TrafficLightView : UserControl, INotifyPropertyChanged
+    {
+        private BitmapImage _imageSource;
+        public BitmapImage ImageSource
+        {
+            get => _imageSource;
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged("ImageSource");
+            }
+        }
+
+        private int _lightState;
+        public int LightState
+        {
+            get => _lightState;
+            set
+            {
+                _lightState = value;
+                OnPropertyChanged("LightState");
+            }
+        }
+
+        private TrafficLightView _slaveLight;
+        public TrafficLightView SlaveLight
+        {
+            get => _slaveLight;
+            set
+            {
+                _slaveLight = value;
+
+                _slaveLight.InitialiseSlave(masterLight, 2);
+                OnPropertyChanged("SlaveLight");
+            }
+        }
+
+        private void OnPropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        DispatcherTimer stateTimer = new DispatcherTimer();
+        private static TrafficLightView masterLight = null;
+        private static int lightCount;
+        private int id;
+
+        public TrafficLightView()
+        {
+            DataContext = this;
+            InitializeComponent();
+
+            id = ++lightCount;
+            LightState = 0;
+
+            string imagePath = "\\Images\\greenlight.png";
+            this.ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+
+            stateTimer.Tick += new EventHandler(StateTimer_Tick);
+            stateTimer.Interval = new TimeSpan(0, 0, 0, 0, 333);
+
+            if (masterLight == null)
+                masterLight = this;
+        }
+
+        public void InitialiseSlave(TrafficLightView trafficLight, int State)
+        {
+            _slaveLight = trafficLight;
+            LightState = State;
+            string imagePath = "\\Images\\redlight.png";
+            this.ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+        }
+
+        public void NextState()
+        {
+            stateTimer.Start();
+        }
+
+        private void StateTimer_Tick(object sender, EventArgs e)
+        {
+            string imagePath = "\\Images\\";
+            LightState++;
+            switch (LightState)
+            {
+                case 0:
+                    imagePath += "greenlight.png";
+                    ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+                    stateTimer.Stop();
+                    break;
+
+                case 1:
+                    imagePath += "amberlight.png";
+                    ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+                    break;
+
+                case 2:
+                    imagePath += "redlight.png";
+                    ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+                    stateTimer.Stop();
+                    SlaveLight.NextState();
+                    break;
+
+                default:
+                    imagePath += "redamberlight.png";
+                    ImageSource = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+                    LightState = -1;
+                    break;
+            }
+        }
+    }
+}
